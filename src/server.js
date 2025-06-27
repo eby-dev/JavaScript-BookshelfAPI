3fb982a97c3fdc5524c3f78c0757f73b3e703994
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2025-06-27 21:50:31 +0700

    feat: remove host & use port: 9000

--- src/server.js
+++ src/server.js
@@ -6,8 +6,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 8080,
-    //host: 'localhost',
+    port: 9000,
     routes: {
       cors: {
         origin: ['*'],
